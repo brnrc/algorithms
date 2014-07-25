832b281ead1187b4d979e562c614721839349081
Bruno Cardoso <brucardoso2@gmail.com>
2014-07-25 13:09:01 -0300

    Updated BST InvariantSets to use Java7 Diamond notation.
    - Also removed a long test, since the algorithm is 2^n.

--- src/algorithms/bst/InvariantSets.java
+++ src/algorithms/bst/InvariantSets.java
@@ -34,16 +34,16 @@ public class InvariantSets {
 
     // StackOverflow: http://stackoverflow.com/questions/1670862/obtaining-a-powerset-of-a-set-in-java
     private static <T> Set<Set<T>> powerSet(Set<T> originalSet) {
-        Set<Set<T>> sets = new HashSet<Set<T>>();
+        Set<Set<T>> sets = new HashSet<>();
         if (originalSet.isEmpty()) {
             sets.add(new HashSet<T>());
             return sets;
         }
-        List<T> list = new ArrayList<T>(originalSet);
+        List<T> list = new ArrayList<>(originalSet);
         T head = list.get(0);
-        Set<T> rest = new HashSet<T>(list.subList(1, list.size()));
+        Set<T> rest = new HashSet<>(list.subList(1, list.size()));
         for (Set<T> set : powerSet(rest)) {
-            Set<T> newSet = new HashSet<T>();
+            Set<T> newSet = new HashSet<>();
             newSet.add(head);
             newSet.addAll(set);
             sets.add(newSet);
--- test/algorithms/bst/InvariantSetsTest.java
+++ test/algorithms/bst/InvariantSetsTest.java
@@ -13,9 +13,4 @@ public class InvariantSetsTest {
         Assert.assertEquals(90, InvariantSets.countSets(new int[]{12, 10, 0, 4, 0, 6, 3, 8, 9, 14, 1, 5, 6, 12, 5}));
         Assert.assertEquals(143, InvariantSets.countSets(new int[]{0, 7, 5, 13, 9, 3, 8, 9, 1, 5, 0, 4, 0, 10, 10}));
     }
-
-    @Test
-    public void testCountBigSet() throws Exception {
-        Assert.assertEquals(70368744177664L, InvariantSets.countSets(new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 27, 26, 25, 28, 29, 30, 31, 32, 35, 34, 37, 36, 33, 38, 39, 40, 41, 48, 43, 44, 45, 46, 47, 42, 49}));
-    }
 }
