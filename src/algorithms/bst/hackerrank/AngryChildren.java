6c380d729ae2a63fc8c44b8577c95dd340e352b9
Bruno Cardoso <brucardoso2@gmail.com>
2014-08-25 14:30:48 -0300

    BST Angry Children from HackerRank.

---
+++ src/algorithms/bst/hackerrank/AngryChildren.java
@@ -0,0 +1,33 @@
+package algorithms.bst.hackerrank;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Created by Bruno Cardoso on 25/08/2014.
+ */
+public class AngryChildren {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int N = scan.nextInt();
+        int K = scan.nextInt();
+
+        List<Integer> packs = new ArrayList<>(N);
+        for (int i = 0; i < N; i++) {
+            packs.add(scan.nextInt());
+        }
+
+        Collections.sort(packs);
+
+        long result = 10000000000L;
+        for (int i = K-1; i < N; i++) {
+            long diff = packs.get(i) - packs.get(i-K+1);
+            if(diff < result) result = diff;
+        }
+
+        System.out.println(result);
+    }
+}
